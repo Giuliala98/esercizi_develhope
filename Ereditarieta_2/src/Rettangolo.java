1ae5fecf26e496473e0a07a97c5f396520b311db
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-01-25 23:53:17 +0100

    esercizio Ereditarieta_2 svolto

---
+++ Ereditarieta_2/src/Rettangolo.java
@@ -0,0 +1,31 @@
+public class Rettangolo extends Forma {
+    private int b;
+    private int h;
+
+    public Rettangolo(int b, int h) {
+        this.b = b;
+        this.h = h;
+    }
+
+    @Override
+    public double calcolaArea() {
+        return b * h;
+
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+}
