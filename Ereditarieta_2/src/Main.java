e2b47e2841ad52bb9b3f98d7095704587529cdbc
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-01-25 23:59:18 +0100

    esercizio Ereditarieta_2 svolto

--- Ereditarieta_2/src/Main.java
+++ Ereditarieta_2/src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Rettangolo rettangolo = new Rettangolo(2, 3);
-        Triangolo triangolo = new Triangolo(2,3);
         System.out.println(rettangolo.calcolaArea());
-        System.out.println(triangolo.calcolaArea());
     }
 }
