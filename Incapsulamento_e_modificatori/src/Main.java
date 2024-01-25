48f1c8254a9016a7c82b1c0c6717e480267522a1
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-01-25 23:47:00 +0100

    esercizio Incapsulamento_e_modificatori svolto

---
+++ Incapsulamento_e_modificatori/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Auto audi = new Auto(1600, "EM252CI", "Audi","A1");
+        System.out.println(audi.getCilindrata());
+        System.out.println(audi.getTarga());
+        System.out.println(audi.getMarca());
+        System.out.println(audi.getModello());
+    }
+}
