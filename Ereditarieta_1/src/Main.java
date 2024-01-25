10cef9b72d81091f1fe01d8d4548fe15d4efff90
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-01-25 23:50:50 +0100

    esercizio Ereditarieta_1 svolto

---
+++ Ereditarieta_1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Animale animale = new Animale();
+        Gatto cat = new Gatto();
+        animale.faiIlVerso();
+        cat.faiIlVerso();
+    }
+}
