5702f15d6bf63b6b1f3ec40666c538653f8d4820
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-02-13 23:19:48 +0100

    svolto esercizio date 2

---
+++ Date2/src/Main.java
@@ -0,0 +1,15 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+// Crea un oggetto OffsetDateTime da questa stringa 2023-03-01T13:00:00Z
+//Formatta la data ottenendo 01 marzo 2023
+//Stampa sulla console
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        String formatData = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"));
+        System.out.println(formatData);
+    }
+}
