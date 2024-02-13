f540fcf73799a13a8859b81ed08cab2a14fbead9
Giulia Lanza <giulia.lanza9816@gmail.com>
2024-02-13 23:09:25 +0100

    record

---
+++ Record/src/Main.java
@@ -0,0 +1,8 @@
+// Definisci un oggetto Record che rappresenti un Punto con le coordinate x & y
+public class Main {
+    public static void main(String[] args) {
+        Punto p1 = new Punto(2,3);
+        System.out.println(p1);
+        System.out.println();
+    }
+}
---
+++ Record/src/Punto.java
@@ -0,0 +1,3 @@
+public record Punto (int x, int y){
+
+}
